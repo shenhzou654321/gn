f8e44ab6e549648e1c3d3de219c507de85d74fad
Tim Niederhausen <tim@rnc-ag.de>
2016-03-19 08:46:00 +0100

    Remove lastchange action

--- gn_main.cc
+++ gn_main.cc
@@ -14,11 +14,7 @@
 
 // Only the GN-generated build makes this header for now.
 // TODO(brettw) consider adding this if we need it in GYP.
-#if defined(GN_BUILD)
-#include "tools/gn/last_commit_position.h"
-#else
 #define LAST_COMMIT_POSITION "UNKNOWN"
-#endif
 
 namespace {
 
